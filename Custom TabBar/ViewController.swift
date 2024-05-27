8b9ba2d712d218926f0cef7ff7b4c318f769701c
Ethan <q40131125@gmail.com>
2024-05-27 17:48:31 +0800

    update

--- Custom TabBar/ViewController.swift
+++ Custom TabBar/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tabBarView: UIView!
+    
+    @IBOutlet weak var contentView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        customizeTabBar()
+        DispatchQueue.main.async {
+            self.firstPage()
+        }
     }
 
+    func customizeTabBar () {
+        tabBarView.layer.cornerRadius = tabBarView.frame.height / 2
+        tabBarView.clipsToBounds = true
+    }
+    func firstPage() {
+        guard let firstPage = self.storyboard?.instantiateViewController(identifier: "FirstViewController") as? FirstViewController else { return }
+        self.addChild(firstPage)
+        firstPage.view.frame = contentView.bounds
+        contentView.addSubview(firstPage.view)
+        firstPage.didMove(toParent: self)
+    }
 
+    func removeCurrentChildViewController() {
+        for child in children {
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
+        }
+    }
+    func addChildViewController(withIdentifier identifier: String) {
+        guard let childViewController = self.storyboard?.instantiateViewController(identifier: identifier) as? UIViewController else { return }
+        self.addChild(childViewController)
+        childViewController.view.frame = contentView.bounds
+        contentView.addSubview(childViewController.view)
+        childViewController.didMove(toParent: self)
+    }
+    @IBAction func tapToChangeViews(_ sender: UIButton) {
+        let tag = sender.tag
+        removeCurrentChildViewController()
+        print(tag)
+        switch tag {
+        case 1:
+            firstPage()
+        case 2:
+            addChildViewController(withIdentifier: "SecondViewController")
+        case 3:
+            addChildViewController(withIdentifier: "ThirdViewController")
+        case 4:
+            addChildViewController(withIdentifier: "FourthViewController")
+        default:
+            break
+        }
+    }
 }
-
+    
